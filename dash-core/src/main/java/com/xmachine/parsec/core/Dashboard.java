59d0903a4113ade78ea7d387d0fc8dc31b2f51a0
skywalker <lipeeskywalker@gmail.com>
2025-07-14 21:37:58 -0300

    feat: show data

--- dash-core/src/main/java/com/xmachine/parsec/core/Dashboard.java
+++ dash-core/src/main/java/com/xmachine/parsec/core/Dashboard.java
@@ -1,13 +1,29 @@
 package com.xmachine.parsec.core;
 
-public class Dashboard {
-    private static Server server;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.xmachine.parsec.telemetry.MotorRegistry;
+import com.xmachine.parsec.telemetry.RobotPose;
 
+public class Dashboard {
+
+    static RobotPose robotPose;
+    static MotorRegistry motorRegistry;
     public static void init() {
+        robotPose = new RobotPose();
+        motorRegistry = new MotorRegistry();
         try {
-            server = new Server(8082); // change if needed
+            new Server(8082);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void setRobotPose(double x, double y, double heading) {
+        robotPose.setPose(x, y, heading);
+    }
+
+    public static void addMotor(DcMotorEx motor) {
+        motorRegistry.addMotor(motor);
     }
 }
